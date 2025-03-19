5dd663ceca2b3e434e8e7b55f6404ab895997239
Jiayuuuuuuuuu <preveenong@gmail.com>
2025-03-19 23:10:38 +0800

    Update ReactSider.js

--- src/components/Navigation/ReactSider.js
+++ src/components/Navigation/ReactSider.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
-
-import { iconStyle } from '../../styles'
+import { Layout, Menu, Button } from 'antd'
 import {
-  AlertOutlined,
-  DollarOutlined,
-  BankOutlined,
-  GlobalOutlined
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  DollarOutlined
 } from '@ant-design/icons'
 import { connect } from 'react-redux'
 
 const { Sider } = Layout
-const { SubMenu } = Menu
 
 class ReactSider extends Component {
+  state = {
+    collapsed: false
+  }
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
   render () {
     return (
       <React.Fragment>
@@ -22,76 +28,48 @@ class ReactSider extends Component {
           breakpoint="md"
           collapsedWidth="0"
           width={240}
-          style={{ background: '#fff' }}>
+          style={{ background: '#fff' }}
+          collapsed={this.state.collapsed}
+          collapsible
+          trigger={null}
+        >
+          <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '12px' }}>
+            <Button
+              type="text"
+              onClick={this.toggleCollapsed}
+              style={{ marginBottom: 16 }}
+            >
+              {this.state.collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            </Button>
+          </div>
           <Menu
             mode="inline"
             selectedKeys={[this.props.sider_selected]}
-            defaultOpenKeys={['menu-exchange-rates', 'menu-updates', 'menu-coins', 'menu-events', 'menu-global', 'menu-exchanges']}
             style={{ height: '100%', borderRight: 0 }}
           >
-            {/* Status updates */}
-            <SubMenu
-              key="menu-updates"
-              title={
-                <span>
-                  <AlertOutlined style={iconStyle}/>General
-                </span>
-              }
-            >
-              <Menu.Item key="asset-platforms"><Link to='/asset-platforms'>Asset Platforms</Link></Menu.Item>
-              <Menu.Item key="trending-coins"><Link to='/trending-coins'>Trending coins</Link></Menu.Item>
-            </SubMenu>
-            {/* Coin Sub Menu */}
-            <SubMenu
-              key="menu-coins"
-              title={
-                <span>
-                  <DollarOutlined style={iconStyle}/>Coins
-                </span>
-              }
-            >
-              <Menu.Item key="coin-list"><Link to='/coins'>Coin Listing</Link></Menu.Item>
-              <Menu.Item key="coin-detail"><Link to='/coins/bitcoin'>Coin Detail View</Link></Menu.Item>
-            </SubMenu>
-
-            {/* Exchanges Menu */}
-            <SubMenu
-              key="menu-exchanges"
-              title={
-                <span>
-                  <BankOutlined style={iconStyle}/>
-                            Exchanges
-                </span>
-              }>
-              <Menu.Item key="exchanges-list"><Link to='/exchanges/list'>Exchanges Listing</Link></Menu.Item>
-            </SubMenu>
-
-            {/* Exchange Rates Menu */}
-            <SubMenu
-              key="menu-exchange-rates"
-              title={
-                <span>
-                  <DollarOutlined style={iconStyle}/>
-                            Exchange Rates
-                </span>
-              }>
-              <Menu.Item key="exchange-rates-list"><Link to='/exchange-rates'>Exchange Rates Listing</Link></Menu.Item>
-            </SubMenu>
-
-            {/* Global Sub Menu */}
-            <SubMenu
-              key="menu-global"
-              title={
-                <span>
-                  <GlobalOutlined style={iconStyle}/>
-                            Global
-                </span>
-              }
-            >
-              <Menu.Item key="global-main"><Link to='/global'>Data</Link></Menu.Item>
-            </SubMenu>
+            <Menu.Item key="coin-list" icon={<DollarOutlined />}>
+              <Link to="/coins">Coin Listing</Link>
+            </Menu.Item>
+            <Menu.Item key="trending-coins" icon={<DollarOutlined />}>
+              <Link to="/trending-coins">Trending Coins</Link>
+            </Menu.Item>
           </Menu>
         </Sider>
+        {/* Add mobile-only toggle button */}
+        {this.state.collapsed && (
+          <Button
+            type="primary"
+            onClick={this.toggleCollapsed}
+            style={{
+              position: 'fixed',
+              bottom: '20px',
+              left: '20px',
+              zIndex: 1000,
+              display: window.innerWidth <= 768 ? 'block' : 'none'
+            }}
+            icon={<MenuUnfoldOutlined />}
+          />
+        )}
       </React.Fragment>
     )
   }
